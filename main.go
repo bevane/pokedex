dad8c9cedbbec8cd4a257a1a3f2be7c241718edd
bevane <bevane@live.com>
2024-09-30 16:45:23 +0700

    feat: add REPL

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
-	fmt.Println("Welcome to the pokedex")
+	scanner := bufio.NewScanner(os.Stdin)
+	nameToCliCommand := getCLICommandMap()
+	for {
+		var command cliCommand
+		var err error
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		command, ok := nameToCliCommand[input]
+		if !ok {
+			fmt.Printf("%s is not a valid command", input)
+			command = nameToCliCommand["help"]
+		}
+		err = command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func getCLICommandMap() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Print("\nWelcome to the Pokedex.\nUsage:\n\n")
+	commandMap := getCLICommandMap()
+	for _, command := range commandMap {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
